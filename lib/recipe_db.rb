477ddf5943946f3337baf9d57d0371999985b65b
sofiajeanne <sofia.rogg@gmail.com>
2016-09-08 14:57:22 -0400

    completed retrieval class

--- lib/recipe_db.rb
+++ lib/recipe_db.rb
@@ -14,7 +14,6 @@ require 'sqlite3'
 # DB = SQLite3::Database.open "recipes.sqlite"
 # DB.execute "INSERT INTO recipe VALUES(1, 'Korean Cucumber Salad')"
 
-
 DB = SQLite3::Database.open (File.expand_path('../recipes.sqlite', __FILE__))
 
 class AddRecipe
@@ -75,18 +74,67 @@ class AddRecipe
     recipe_instructions
     add_recipe
     add_ingredients
+    add_rels
   end
 
 end
+
 
 class GetRecipe
 
-  def display_recipe(recipe_name)
-    DB.execute "SELECT"
+  def get_recipe_input
+    puts "Please enter the recipe you're looking for"
+    @recipe_name = $stdin.gets.chomp
+    #@recipe_name = "Korean Cucumber Salad"
+  end
+
+  def get_recipe_id
+    @recipe_id = DB.execute "SELECT recipe_id FROM recipes WHERE recipe_name='#{@recipe_name}'"
+    @recipe_id = @recipe_id[0][0]
+  end
+
+  def get_instructions
+    @instructions = DB.execute "SELECT instructions FROM recipes WHERE recipe_name='#{@recipe_name}'"
+  end
+
+  def get_ingredients
+    @ingredient_id = DB.execute "SELECT ing_id FROM recipe_rel WHERE recipe_id=#{@recipe_id}"
+    @ingredient_id.map! do |ing|
+      ing[0]
+    end
+    @ingredient_names = []
+    @ingredient_id.each do |id|
+      ingredient = DB.execute "SELECT ing FROM ingredients WHERE ing_id=#{id}"
+      @ingredient_names << ingredient
+    end
+    @ingredient_names.map! do |ing|
+      ing[0][0]
+    end
+    @ing_hash = Hash[@ingredient_id.zip @ingredient_names]
+    @ingredient_list = DB.execute "SELECT ing_id, amount, unit FROM recipe_rel WHERE recipe_id=#{@recipe_id}"
+    @ingredient_list.each do |ing|
+      ing[0] = @ing_hash[ing[0]]
+      ing.rotate!
+    end
+    @ingredient_list.map! do |ing|
+      ing.join(" ")
+    end
+  end
+
+  def fetch_recipe
+    get_recipe_input
+    get_recipe_id
+    get_instructions
+    get_ingredients
+  end
+
+  def display_recipe
+    puts @recipe_name
+    puts @ingredient_list
+    puts @instructions
   end
 
 end
-
 
 =begin
 open ingredients table
--- lib/tester.rb
+++ lib/tester.rb
@@ -6,11 +6,12 @@ require 'sqlite3'
 DB = SQLite3::Database.open (File.expand_path('../recipes.sqlite', __FILE__))
 
 def get_recipe_name
-  #puts "Please enter the recipe you're looking for"
-  #@recipe_name = $stdin.gets.chomp
-  @recipe_name = 'Korean Cucumber Salad'
+  puts "Please enter the recipe you're looking for"
+  recipe_name = $stdin.gets.chomp
+  @recipe_name = recipe_name
 end
 
+#fix whatever is going on here
 def get_recipe_id
   @recipe_id = DB.execute "SELECT recipe_id FROM recipes WHERE recipe_name='#{@recipe_name}'"
   @recipe_id = @recipe_id[0][0]
@@ -20,7 +21,6 @@ def get_instructions
   @instructions = DB.execute "SELECT instructions FROM recipes WHERE recipe_name='#{@recipe_name}'"
 end
 
-# method in progress
 def get_ingredients
   @ingredient_id = DB.execute "SELECT ing_id FROM recipe_rel WHERE recipe_id=#{@recipe_id}"
   @ingredient_id.map! do |ing|
@@ -34,19 +34,32 @@ def get_ingredients
   @ingredient_names.map! do |ing|
     ing[0][0]
   end
-  ing_hash = Hash[@ingredient_id.zip @ingredient_names]
-  puts ing_hash
-  #@ingredients = DB.execute "SELECT ing_id, amount, unit FROM recipe_rel WHERE recipe_id=#{@recipe_id}"
+  @ing_hash = Hash[@ingredient_id.zip @ingredient_names]
+  @ingredient_list = DB.execute "SELECT ing_id, amount, unit FROM recipe_rel WHERE recipe_id=#{@recipe_id}"
+  @ingredient_list.each do |ing|
+    ing[0] = @ing_hash[ing[0]]
+    ing.rotate!
+  end
+  @ingredient_list.map! do |ing|
+    ing.join(" ")
+  end
 end
 
-def run
+def fetch_recipe
   get_recipe_name
   get_recipe_id
   get_instructions
   get_ingredients
 end
 
-run
+def display_recipe
+  fetch_recipe
+  puts @recipe_name
+  puts @ingredient_list
+  puts @instructions
+end
+
+puts fetch_recipe
 
 =begin
 @ingredient_names.each_with_index do |ing, i|
